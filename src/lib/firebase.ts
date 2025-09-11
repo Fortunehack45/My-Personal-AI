51ac56f6ca4038ea72cb6a1a49bb68e1ca8f08e5
Fortune Domination <fortunedomination@gmail.com>
2025-09-11 21:54:34 +0000

    Cannot read properties of undefined (reading 'trimEnd')

---
+++ src/lib/firebase.ts
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCmARj_o7ho4WljRtdhNaaoyzq_z0zzK2o",
+  authDomain: "futa-score-navigator.firebaseapp.com",
+  projectId: "futa-score-navigator",
+  storageBucket: "futa-score-navigator.appspot.com",
+  messagingSenderId: "580172904649",
+  appId: "1:580172904649:web:45365c84de0a0085a9b518"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
