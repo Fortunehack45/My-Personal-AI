cabd06c80a36bf15237c0c7b65a7ccee95d8cd56
Fortune Domination <fortunedomination@gmail.com>
2025-09-12 14:49:22 +0000

    This issue is from the admin page: Error: 2 UNKNOWN: Getting metadata fr

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,6 +5,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiVersion: 'v1beta', apiKey: 'AIzaSyB_UocuqMfM38zAFq09v7i3f9LsKt3l61A'})],
+  plugins: [googleAI({apiVersion: 'v1beta'})],
   model: 'googleai/gemini-2.5-flash',
 });
