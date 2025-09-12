c8a0fc533deee98346f480466869d3fddd464022
Fortune Domination <fortunedomination@gmail.com>
2025-09-12 14:43:44 +0000

    Error: 2 UNKNOWN: Getting metadata from plugin failed with error: Could

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -5,6 +5,6 @@ import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
 
 export const ai = genkit({
-  plugins: [googleAI({apiVersion: 'v1beta'})],
+  plugins: [googleAI({apiVersion: 'v1beta', apiKey: 'AIzaSyDOe1T4DIbqnc7GvcYf-U3IvcPPLL1K5zs'})],
   model: 'googleai/gemini-2.5-flash',
 });
