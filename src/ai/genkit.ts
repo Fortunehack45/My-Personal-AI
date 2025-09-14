05a6cb41e5c250b59966d65a28017948c6750ba5
Fortune Domination <fortunedomination@gmail.com>
2025-09-14 23:02:21 +0000

    You did not change anything

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,6 +1,6 @@
 import {genkit} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
-import {nextPlugin} from '@genkit-ai/next';
+import {nextPlugin} from '@genkit-ai/next/app';
 
 export const ai = genkit({
   plugins: [googleAI(), nextPlugin()],
